cf4a3228bd35323529db6c48f7425f71a6aaf2f9
vike0906 <liujialong688@outlook.com>
2018-05-30 11:21:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+study for spring boot.
---
+++ src/main/java/com/hengxunda/vike0906/Vike0906Application.java
@@ -0,0 +1,12 @@
+package com.hengxunda.vike0906;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vike0906Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vike0906Application.class, args);
+    }
+}
